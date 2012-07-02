71e90eaa29d4b61951959d614ffbd700045c9189
Sergey (dmi3s) Dmitriev <dmi3evsv@gmail.com>
2012-07-02 10:49:43 +0400

    BinTree.reverse

--- BinTree.hs
+++ BinTree.hs
@@ -1,5 +1,8 @@
 module BinTree where
 
-data BinTree a = Leaf a
-               | Node (BinTree a) (BinTree a)
-      deriving (Show, Eq)
+data BinTree a = Leaf a | Node (BinTree a) (BinTree a)
+               deriving (Show, Eq)
+
+reverse :: BinTree a -> BinTree a
+reverse (Leaf a)   = Leaf a
+reverse (Node x y) = Node (BinTree.reverse y) (BinTree.reverse x)
