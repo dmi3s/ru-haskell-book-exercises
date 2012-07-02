d05b9d95587fa16a6a1822facfa48db337898d19
Sergey (dmi3s) Dmitriev <dmi3evsv@gmail.com>
2012-07-02 10:53:47 +0400

    BinTree.depth

--- BinTree.hs
+++ BinTree.hs
@@ -1,8 +1,14 @@
 module BinTree where
+import Nat
 
 data BinTree a = Leaf a | Node (BinTree a) (BinTree a)
                deriving (Show, Eq)
 
 reverse :: BinTree a -> BinTree a
-reverse (Leaf a)   = Leaf a
+reverse (Leaf e)   = Leaf e
 reverse (Node x y) = Node (BinTree.reverse y) (BinTree.reverse x)
+
+
+depth :: BinTree a -> Nat
+depth (Leaf e)   = Zero
+depth (Node x y) = Succ (max (depth x) (depth y))
